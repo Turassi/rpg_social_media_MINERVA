ad9050251057bf2d15060bbf0461002aa9c2bef0
Turassi <christian.turassi@gmail.com>
2024-05-15 19:31:09 -0300

    RPG_SOCIAL_MEDIA

---
+++ src/main/java/br/univille/rpg_social_media/Startup.java
@@ -0,0 +1,24 @@
+package br.univille.rpg_social_media;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.event.EventListener;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.stereotype.Component;
+import br.univille.rpg_social_media.entity.Usuário;
+import br.univille.rpg_social_media.service.UsuárioService;
+
+@Component
+public class Startup {
+    @Autowired
+    private UsuárioService service;
+    
+    public void onApplicationEvent(ContextRefreshedEventevent event){
+
+        var usuário1 = new Usuário();
+        usuário1.setNome("Zézin");
+        usuário1.setEmail("Zézin@zé");
+        usuário1.setSenha("zé12345");
+        service.save(usuário1);
+        
+    }
+}
